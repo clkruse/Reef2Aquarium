6563f428c65cdb837e0cd3e98fd5d046121ea3e9
Caleb Kruse <ckruse@leapmotion.com>
2016-09-27 15:27:17 -0700

    Added a mapSettings variable where I can set default position, etc.

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 window.onload = function() {
     var milestones;
+    var mapSettings = {
+        shareable: true,
+        title: false,
+        description: true,
+        search: false,
+        tiles_loader: true,
+        center_lat: 18,
+        center_lon: 180,
+        zoom: 3,
+        layer_selector:true,
+        cartodb_logo: false,
+        legends: false,
+        mobile_layout: true
+    };
     
-    cartodb.createVis('map', 'http://clkruse.carto.com/api/v2/viz/f4537124-51f2-11e6-8b6c-0ee66e2c9693/viz.json',{ zoomControl: false })
+    cartodb.createVis('map', 'http://clkruse.carto.com/api/v2/viz/f4537124-51f2-11e6-8b6c-0ee66e2c9693/viz.json',mapSettings)
     .done(function(vis, layers) {
         
-        var map = vis.getNativeMap();
-
+        // Returns the native map object being used (e.g. a L.Map object for Leaflet)
+        var map = vis.getNativeMap
+        
         var seq = O.Sequential();
         O.Keys().left().then(seq.prev, seq);
         O.Keys().right().then(seq.next, seq);
