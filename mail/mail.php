90d592356360235817f38cf79b88987dc3c41cce
Caleb K <caleb.krs@gmail.com>
2018-03-24 22:43:01 -0700

    Healthy Trade V1

--- mail/mail.php
+++ mail/mail.php
@@ -20,14 +20,14 @@ $sendingAccountUsername     = 'email@domain.com';
 $sendingAccountPassword     = 'p@55w0rd';
 
 // Recipient (To:) Details  - Change this to the email details of who will receive all the emails from the website.
-$recipientEmail             = 'email@yourdomain.com'; // REQUIRED value.
-$recipientName              = 'John Doe';             // REQUIRED value.
+$recipientEmail             = 'caleb.krs@gmail.com'; // REQUIRED value.
+$recipientName              = 'Reef to Aquarium';             // REQUIRED value.
 
 // Email details            - Change these to suit your website needs
 $emailSubject               = 'A message from Your Website'; // REQUIRED value. Subject of the email that the recipient will see
 $websiteName                = 'Your Website';                // REQUIRED value. This is used when a name or email is not collected from the website form
 
-$timeZone					= 'Australia/Melbourne';         // OPTIONAL, but some servers require this to be set. 
+$timeZone					= 'America/Los_Angeles';         // OPTIONAL, but some servers require this to be set.
                                                              // See a list of all supported timezones at: http://php.net/manual/en/timezones.php
  /*----------------------------------------------------------------------------*\
 |*  You do not need to edit anything below this line, the rest is automatic.    *|
